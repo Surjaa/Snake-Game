82b8bc3402e4a92ed3914502351fa147b664a19a
Gurwinder Parhar <96934565+Surjaa@users.noreply.github.com>
2025-02-26 14:35:12 +0530

    game.py

--- Snake Game/game.py
+++ Snake Game/game.py
@@ -11,10 +11,28 @@ red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
 
-# Game display dimensions
-dis_width = 800
-dis_height = 600
-dis = pygame.display.set_mode((dis_width, dis_height))
+# Determine screen size dynamically or use fixed size for desktop
+info = pygame.display.Info()
+if sys.platform in ['win32', 'darwin']:  # Checks if the OS is Windows or MacOS
+    dis_width = min(800, info.current_w)  # Caps the width at 800 for desktops
+    dis_height = min(600, info.current_h)  # Caps the height at 600 for desktops
+else:
+    # Mobile and other platforms use breakpoints
+    screen_width = info.current_w
+    if screen_width <= 480:
+        dis_width = 320
+        dis_height = 480
+    elif screen_width <= 640:
+        dis_width = 481
+        dis_height = 640
+    elif screen_width <= 768:
+        dis_width = 641
+        dis_height = 768
+    else:
+        dis_width = 769
+        dis_height = 1024
+
+dis = pygame.display.set_mode((dis_width, dis_height), pygame.RESIZABLE)
 pygame.display.set_caption('Snake Game')
 
 clock = pygame.time.Clock()
@@ -36,7 +54,8 @@ food_img = pygame.transform.scale(food_img, (snake_block, snake_block))
 num_obstacles = 5
 obstacles = []
 last_update_time = pygame.time.get_ticks()
-update_interval = 20000  # 20000 milliseconds (20 seconds)
+update_interval = 20000
+
 
 def create_obstacles():
     global obstacles
@@ -47,19 +66,24 @@ def create_obstacles():
             obstacle = pygame.Rect(random.randint(0, dis_width - 30), random.randint(0, dis_height - 30), 30, 30)
         obstacles.append(obstacle)
 
+
 create_obstacles()
+
 
 def our_snake(snake_block, snake_list):
     for x in snake_list:
         pygame.draw.ellipse(dis, blue, [x[0], x[1], snake_block, snake_block])
 
+
 def text_objects(text, font):
     textSurface = font.render(text, True, white)
     return textSurface, textSurface.get_rect()
 
+
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
     dis.blit(mesg, [dis_width / 6, dis_height / 3])
+
 
 def game_intro():
     intro = True
@@ -67,24 +91,26 @@ def game_intro():
         dis.fill(black)
         largeText = pygame.font.SysFont("comicsansms", 115)
         TextSurf, TextRect = text_objects("Snake Game", largeText)
-        TextRect.center = ((dis_width / 2), (dis_height / 2))
+        TextRect.center = ((dis_width / 2), (dis_height / 3))
         dis.blit(TextSurf, TextRect)
 
-        pygame.draw.rect(dis, green, [150, 450, 100, 50])
+        play_button = pygame.Rect(dis_width / 2 - 50, dis_height / 2, 100, 50)
+        pygame.draw.rect(dis, green, play_button)
         smallText = pygame.font.SysFont("comicsansms", 20)
         textSurf, textRect = text_objects("Play", smallText)
-        textRect.center = ((150 + (100 / 2)), (450 + (25)))
+        textRect.center = (play_button.centerx, play_button.centery)
         dis.blit(textSurf, textRect)
 
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse = pygame.mouse.get_pos()
-                if 150 + 100 > mouse[0] > 150 and 450 + 50 > mouse[1] > 450:
+                if play_button.collidepoint(mouse):
                     intro = False
-            if event.type == pygame.QUIT:
+            if event.type is pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
 
 def gameLoop():
     global last_update_time
@@ -95,7 +121,7 @@ def gameLoop():
     y1 = dis_height / 2
     x1_change = 0
     y1_change = 0
-    snake_List = [[x1, y1]]
+    snake_list = [[x1, y1]]
     length_of_snake = 1
 
     foodx = round(random.randrange(0, dis_width - snake_block) / snake_block) * snake_block
@@ -109,32 +135,42 @@ def gameLoop():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_c:
                         gameLoop()
-                    if event.key == pygame.K_q:
+                    elif event.key == pygame.K_q:
                         pygame.quit()
                         sys.exit()
-
-        current_time = pygame.time.get_ticks()
-        if current_time - last_update_time > update_interval:
-            create_obstacles()
-            last_update_time = current_time
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
+                if event.key == pygame.K_LEFT and x1_change == 0:
                     x1_change = -snake_block
                     y1_change = 0
-                elif event.key == pygame.K_RIGHT:
+                elif event.key == pygame.K_RIGHT and x1_change == 0:
                     x1_change = snake_block
                     y1_change = 0
-                elif event.key == pygame.K_UP:
+                elif event.key == pygame.K_UP and y1_change == 0:
                     y1_change = -snake_block
                     x1_change = 0
-                elif event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_DOWN and y1_change == 0:
                     y1_change = snake_block
                     x1_change = 0
+            elif event.type == pygame.FINGERDOWN:
+                if event.x < 0.5:
+                    if event.dx < 0:
+                        x1_change = -snake_block
+                        y1_change = 0
+                    else:
+                        x1_change = snake_block
+                        y1_change = 0
+                else:
+                    if event.dy < 0:
+                        y1_change = -snake_block
+                        x1_change = 0
+                    else:
+                        y1_change = snake_block
+                        x1_change = 0
 
         x1 += x1_change
         y1 += y1_change
@@ -146,27 +182,34 @@ def gameLoop():
         dis.fill(black)
         dis.blit(food_img, (foodx, foody))
         for ob in obstacles:
-            pygame.draw.rect(dis, red, ob)  # Draw each obstacle
-        our_snake(snake_block, snake_List)
+            pygame.draw.rect(dis, red, ob)
+        our_snake(snake_block, snake_list)
         pygame.display.update()
 
-        snake_Head = [x1, y1]
-        snake_List.append(snake_Head)
-        if len(snake_List) > length_of_snake:
-            del snake_List[0]
+        snake_head = [x1, y1]
+        snake_list.append(snake_head)
+        if len(snake_list) > length_of_snake:
+            del snake_list[0]
 
-        if x1 == foodx and y1 == foody:
+        # Check collision with the snake itself
+        for segment in snake_list[:-1]:
+            if segment == snake_head:
+                pygame.mixer.Sound.play(game_over_sound)
+                game_close = True
+
+        if abs(x1 - foodx) < snake_block and abs(y1 - foody) < snake_block:
             pygame.mixer.Sound.play(eat_sound)
             foodx = round(random.randrange(0, dis_width - snake_block) / snake_block) * snake_block
             foody = round(random.randrange(0, dis_height - snake_block) / snake_block) * snake_block
             length_of_snake += 1
 
         for ob in obstacles:
-            if ob.collidepoint(snake_Head[0] + snake_block // 2, snake_Head[1] + snake_block // 2):
+            if ob.collidepoint(snake_head[0] + snake_block // 2, snake_head[1] + snake_block // 2):
                 pygame.mixer.Sound.play(game_over_sound)
                 game_close = True
 
         clock.tick(snake_speed)
+
 
 game_intro()
 gameLoop()
